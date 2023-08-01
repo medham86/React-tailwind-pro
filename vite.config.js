59dc1403ffa365f33631eee626558cf4ca7925a1
medham86 <eng.medhathamada@gmail.com>
2023-08-02 02:37:03 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ https://github.com/medham86/React-tailwind-pro.git",
   plugins: [react()],
 })
